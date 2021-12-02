ca343592cd9c2db0fe220d6e57073ba4bbfb8080
Tilo Prütz <tilo@infopark.de>
2021-12-02 07:24:22 +0100

    2021/02

---
+++ 2021/02/2/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, err := ioutil.ReadAll(os.Stdin)
+	reportErr("failed to read standard input", err)
+
+	x := 0
+	y := 0
+	aim := 0
+	for _, line := range strings.Split(strings.Trim(string(input), "\n"), "\n") {
+		tokens := strings.Split(line, " ")
+		if len(tokens) != 2 {
+			reportErr("failed to parse input", fmt.Errorf("wrong token count: %d", len(tokens)))
+		}
+
+		command := tokens[0]
+		amount, err := strconv.Atoi(tokens[1])
+		reportErr("failed to parse input", err)
+
+		switch command {
+		case "forward":
+			x += amount
+			y += aim * amount
+		case "up":
+			aim -= amount
+		case "down":
+			aim += amount
+		default:
+			reportErr("", fmt.Errorf("unexpected command: %s", command))
+		}
+	}
+
+	fmt.Println(x * y)
+}
+
+func reportErr(msg string, err error) {
+	if err != nil {
+		if msg != "" {
+			_, _ = fmt.Fprintln(os.Stderr, msg+":", err)
+		} else {
+			_, _ = fmt.Fprintln(os.Stderr, err)
+		}
+		os.Exit(1)
+	}
+}
