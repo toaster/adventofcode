7bd186c91926dee99d054921d7539b20c57b97bc
Tilo Prütz <tilo@infopark.de>
2021-12-05 09:01:17 +0100

    2021/04

---
+++ 2021/04/1/main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, err := ioutil.ReadAll(os.Stdin)
+	reportErr("failed to read standard input", err)
+
+	var numbers []int
+	var boards []*board
+	var current *board
+	var y int
+	for i, line := range strings.Split(strings.Trim(string(input), "\n"), "\n") {
+		if i == 0 {
+			numbers = parseNumbers(line, ",")
+			continue
+		}
+
+		if line == "" {
+			current = &board{fieldsByValues: map[int]*field{}}
+			boards = append(boards, current)
+			y = 0
+			continue
+		}
+
+		nums := parseNumbers(line, " ")
+		var row []*field
+		for x, num := range nums {
+			f := &field{
+				m: false,
+				v: num,
+				x: x,
+				y: y,
+			}
+			current.fieldsByValues[num] = f
+			row = append(row, f)
+		}
+		current.fields = append(current.fields, row)
+		y++
+	}
+
+	for ni, n := range numbers {
+		for bi, b := range boards {
+			if mark(b, n) {
+				fmt.Println("board", bi+1, "wins at number", ni+1, "which is", n)
+				sum := 0
+				for _, f := range b.fieldsByValues {
+					if !f.m {
+						sum += f.v
+					}
+				}
+				fmt.Println("final score", sum*n)
+				os.Exit(0)
+			}
+		}
+	}
+}
+
+func printBoards(boards []*board) {
+	for _, b := range boards {
+		for _, r := range b.fields {
+			for _, f := range r {
+				m := ' '
+				if f.m {
+					m = 'x'
+				}
+				fmt.Printf("%2d %c ", f.v, m)
+			}
+			fmt.Println()
+		}
+		fmt.Println()
+	}
+}
+
+func mark(b *board, n int) bool {
+	if f := b.fieldsByValues[n]; f != nil {
+		f.m = true
+		donex := true
+		doney := true
+		for i := 0; i < 5; i++ {
+			if !b.fields[f.y][i].m {
+				doney = false
+			}
+
+			if !b.fields[i][f.x].m {
+				donex = false
+			}
+		}
+
+		return donex || doney
+	}
+	return false
+}
+
+func parseNumbers(line string, sep string) (numbers []int) {
+	for _, s := range strings.Split(line, sep) {
+		if s == "" {
+			continue
+		}
+
+		v, err := strconv.Atoi(s)
+		if err != nil {
+			reportErr("failed to parse input", err)
+		}
+
+		numbers = append(numbers, v)
+	}
+	return
+}
+
+type board struct {
+	fieldsByValues map[int]*field
+	fields         [][]*field
+}
+
+type field struct {
+	m bool
+	v int
+	x int
+	y int
+}
+
+func reportErr(msg string, err error) {
+	if err != nil {
+		if msg != "" {
+			_, _ = fmt.Fprintln(os.Stderr, msg+":", err)
+		} else {
+			_, _ = fmt.Fprintln(os.Stderr, err)
+		}
+		os.Exit(1)
+	}
+}
