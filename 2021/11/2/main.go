36bb6df6a28975a2ba5ad015e0cf6e2f30dd0193
Tilo Prütz <tilo@infopark.de>
2021-12-12 13:12:14 +0100

    2021/11

---
+++ 2021/11/2/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/toaster/advent_of_code/internal/io"
+)
+
+func main() {
+	width := 10
+	height := 10
+	c := cavern{}
+	for y, line := range io.ReadLines() {
+		for x, e := range io.ParseInts(line, "") {
+			var neighbours []point
+			if x > 0 {
+				neighbours = append(neighbours, point{x - 1, y})
+				if y > 0 {
+					neighbours = append(neighbours, point{x - 1, y - 1})
+				}
+				if y < height-1 {
+					neighbours = append(neighbours, point{x - 1, y + 1})
+				}
+			}
+			if x < width-1 {
+				neighbours = append(neighbours, point{x + 1, y})
+				if y > 0 {
+					neighbours = append(neighbours, point{x + 1, y - 1})
+				}
+				if y < height-1 {
+					neighbours = append(neighbours, point{x + 1, y + 1})
+				}
+			}
+			if y > 0 {
+				neighbours = append(neighbours, point{x, y - 1})
+			}
+			if y < height-1 {
+				neighbours = append(neighbours, point{x, y + 1})
+			}
+			c[point{x, y}] = &octopus{energy: e, neighbours: neighbours}
+		}
+	}
+
+	step := 0
+	for {
+		flashes := 0
+		for _, o := range c {
+			o.energy++
+		}
+		for _, o := range c {
+			flashes += flashIfNecessary(c, o)
+		}
+		for _, o := range c {
+			if o.flashed {
+				o.flashed = false
+				o.energy = 0
+			}
+		}
+		step++
+		if flashes == width*height {
+			break
+		}
+	}
+	fmt.Println(step)
+}
+
+func flashIfNecessary(c cavern, o *octopus) (flashes int) {
+	if o.energy > 9 && !o.flashed {
+		o.flashed = true
+		flashes++
+		for _, p := range o.neighbours {
+			neighbour := c[p]
+			neighbour.energy++
+			flashes += flashIfNecessary(c, neighbour)
+		}
+	}
+	return
+}
+
+type cavern map[point]*octopus
+
+type point struct {
+	x int
+	y int
+}
+
+type octopus struct {
+	energy     int
+	flashed    bool
+	neighbours []point
+}
