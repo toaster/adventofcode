1913274e80f21f607cea4bacd3ac5473533511b6
Tilo Prütz <tilo.pruetz@justrelate.com>
2023-12-04 08:32:00 +0100

    2015/04

---
+++ 2015/04/2/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"strings"
+
+	"github.com/toaster/advent_of_code/internal/io"
+)
+
+func main() {
+	secret := strings.TrimSpace(io.ReadAll())
+	for i := 1; ; i++ {
+		sum := md5.Sum([]byte(fmt.Sprintf("%s%d", secret, i)))
+		hash := hex.EncodeToString(sum[:])
+		if hash[0] == '0' && hash[1] == '0' && hash[2] == '0' && hash[3] == '0' && hash[4] == '0' && hash[5] == '0' {
+			fmt.Println(i)
+			break
+		}
+	}
+}
